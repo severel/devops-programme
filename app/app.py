406b89b56504a2e0411b18eebbcea9ac96895e7e
Daniel Rankov <daniel@severalclouds.com>
2024-11-05 09:37:54 +0200

    python requirements

--- app/app.py
+++ app/app.py
@@ -9,6 +9,11 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, World!"
 
+@app.route("/sum/<int:a>/<int:b>")
+def calculate_sum(a, b):
+    return str(a + b)
+
+
 
 if __name__ == "__main__":
     app.run(port=os.environ.get("PORT", 5000), host="0.0.0.0")
